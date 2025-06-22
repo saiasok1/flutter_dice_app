d87f74e2a516739377c58b47e840b69338c3be24
saiasok1 <saiasok1k@gmail.com>
2025-06-22 21:14:20 +0530

    🚀first commit\\ added dice images //

--- lib/main.dart
+++ lib/main.dart
@@ -26,24 +26,22 @@ class DiceApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Row(
-          children: [
-            Expanded(
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Image.asset('images/dice1.png'),
               ),
-            ),
-            Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Image.asset('images/dice1.png'),
-                ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
